39a0e7fbfca22bf0bde328480330f2aa3a7fef57
ravisr-28 <ravishankarx213@gmail.com>
2026-04-28 12:41:10 +0530

    Certificate added

--- frontend/src/data/certifications-data.js
+++ frontend/src/data/certifications-data.js
@@ -1,12 +1,23 @@
 export const certifications = [
-    // {
-    //     title: "Programming in Python: A Hands-on Introduction",
-    //     organization: "Codio",
-    //     issueDate: "Feb 2025",
-    //     credentialId: "AWS-123456",
-    //     credentialUrl: "https://coursera.org/share/049242f07cee7fd9cf2c1c7cbceed16f",
-    //     organizationLogo: "/certificates/company-logo/codio-logo.png",
-    //     certificateImage: "/certificates/pythons-fundamentals.png"
-    // },
+//   {
+//     title: "Programming in Python: A Hands-on Introduction",
+//     organization: "Codio",
+//     issueDate: "Feb 2025",
+//     credentialId: "AWS-123456",
+//     credentialUrl:
+//       "https://coursera.org/share/049242f07cee7fd9cf2c1c7cbceed16f",
+//     organizationLogo: "/certificates/company-logo/codio-logo.png",
+//     certificateImage: "/certificates/pythons-fundamentals.png",
+//   },
+  {
+    title:
+      "Training and Internship Certification in Full Stack Web Development with Gen AI + DSA",
+    organization: "Coding Ninjas & IITM Pravartak Technologies Foundation",
+    issueDate: "Feb 2026",
+    credentialId: "DSA4944527629",
+    credentialUrl: "",
+    organizationLogo: "/certificates/coding_ninjas_logo.png",
+    certificateImage: "/certificates/certificate_01.jpg",
+  },
+];
 
-]
